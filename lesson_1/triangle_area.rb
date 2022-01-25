1be3da83a6a5ea806dcff7f04295ae2360827f78
vrtx <s.vertegel@gmail.com>
2022-01-25 13:42:31 +0300

    add triangle area calculator

---
+++ lesson_1/triangle_area.rb
@@ -0,0 +1,9 @@
+puts "Введите длину основания треугольника в см: "
+base = gets.chomp
+a = base.to_i
+
+puts "Введите высоту треугольника в см: "
+height = gets.chomp
+b = height.to_i
+
+puts "Площадь теугольника равна #{ 0.5 * a * b }см."

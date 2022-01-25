a70d908bcdca4777de4e52e35b491c67d1df6272
vrtx <s.vertegel@gmail.com>
2022-01-25 03:33:38 +0300

    add ideal weight calculator

---
+++ lesson_1/ideal_weight.rb
@@ -0,0 +1,13 @@
+puts "Как вас зовут? "
+name = gets.chomp
+
+puts "Укажите ваш рост в сантиметрах: "
+height = gets.chomp
+
+if height.to_i < 150
+    puts "#{name}, ваш оптимальный вес 40кг."
+  else
+    puts "#{name}, ваш оптимальный вес #{((height.to_i - 110) * 1.15).to_i} кг."
+end
+
+

5d20b84486ff1231688d644f02d24c5fbd429aed
vrtx <s.vertegel@gmail.com>
2022-01-25 03:31:30 +0300

    add height.rb

---
+++ lesson_1/height.rb
@@ -0,0 +1,12 @@
+alex_height = 180
+robert_height = 170
+peter_height = 160
+
+if peter_height > alex_height
+    puts "Петя выше Саши"
+  elsif peter_height > robert_height
+    puts "Петя выше Роберта"
+  else
+    puts "Петя ниже всех!"
+end
+

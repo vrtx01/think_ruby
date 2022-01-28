eb6fce3148a60138885571346b25e3995b0aa0b3
vrtx <s.vertegel@gmail.com>
2022-01-28 12:34:41 +0300

    add fibonacci first 100 nums array

---
+++ lesson_2/fibonacci_100.rb
@@ -0,0 +1,14 @@
+def fibonacci(num)
+  first_num = 0
+  second_num = 1
+
+  num.times do # Compute Fibonacci number in the desired position.
+      temp = first_num # Store first_num value for reassign later.
+      first_num = second_num
+      second_num = temp + second_num # Add up previous two numbers in sequence.
+  end
+
+  return first_num
+end
+
+print (1..100).map {|i| fibonacci(i)} # Write first 100 Fibonacci numbers in array.

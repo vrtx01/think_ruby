5920c7bc918d82842da3d731e4372fc19f2dc9a4
vrtx <s.vertegel@gmail.com>
2022-03-24 16:41:59 +0300

    fix style to ruby way

--- lesson_1/height.rb
+++ lesson_1/height.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 alex_height = 180
 robert_height = 170
 peter_height = 160
 
 if peter_height > alex_height
-    puts "Петя выше Саши"
-  elsif peter_height > robert_height
-    puts "Петя выше Роберта"
-  else
-    puts "Петя ниже всех!"
+  puts 'Петя выше Саши'
+elsif peter_height > robert_height
+  puts 'Петя выше Роберта'
+else
+  puts 'Петя ниже всех!'
 end
-

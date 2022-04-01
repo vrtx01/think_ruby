c9a25cd2f345a6cb374f263dd6d071608163e651
vrtx <s.vertegel@gmail.com>
2022-04-01 20:16:42 +0300

    fix rubocop warnings

--- lesson_1/discriminant_and_root.rb
+++ lesson_1/discriminant_and_root.rb
@@ -1,17 +1,19 @@
-puts "Введите коэффициент a: "
+# frozen_string_literal: true
+
+puts 'Введите коэффициент a: '
 a = gets.chomp.to_i
 
-puts "Введите коэффициент b: "
+puts 'Введите коэффициент b: '
 b = gets.chomp.to_i
 
-puts "Введите коэффициент c: "
+puts 'Введите коэффициент c: '
 c = gets.chomp.to_i
 
-d = b ** 2 - 4 * a * c
-if d < 0
-    puts "Результат: #{d}, корней нет."
-  elsif d == 0
-    puts "Результат: #{d}, #{-b / (2 * a)}."
-  elsif d > 0
-    puts "Результат: #{d}, #{(-b - Math.sqrt(d)) / (2 * a)}, #{(-b + Math.sqrt(d)) / (2 * a)}."
+d = b**2 - 4 * a * c
+if d.negative?
+  puts "Результат: #{d}, корней нет."
+elsif d.zero?
+  puts "Результат: #{d}, #{-b / (2 * a)}."
+elsif d.positive?
+  puts "Результат: #{d}, #{(-b - Math.sqrt(d)) / (2 * a)}, #{(-b + Math.sqrt(d)) / (2 * a)}."
 end
